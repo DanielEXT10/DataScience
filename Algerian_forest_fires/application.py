7f7d7140039f28675be357fa3822e6dd9f0081ac
DanielEXT10 <danielalfonso.garcia1196@gmail.com>
2025-04-04 09:05:58 -0600

    ready to deploy V1

--- Algerian_forest_fires/application.py
+++ Algerian_forest_fires/application.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import StandardScaler
 application = Flask(__name__)
 app = application
 
+
+
 ## import ridge regressor and standard scaler pickle
 ridge_model = pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
@@ -19,7 +21,26 @@ def index():
 @app.route("/predictdata", methods = ['GET','POST'])
 def predict_datapoint():
     if request.method == "POST":
-        pass
+        try:
+            #Extract features from form
+            Temperature = float(request.form['Temperature'])
+            RH = float(request.form['RH'])
+            WS = float(request.form['WS'])
+            Rain = float(request.form['Rain'])
+            FFMC = float(request.form['FFMC'])
+            DMC = float(request.form['DMC'])
+            ISI = float(request.form['ISI'])
+            Classes = float(request.form['Classes'])
+            Region = float(request.form['Region'])
+
+            new_data_scaled = standard_scaler.transform([[Temperature,RH,WS,Rain,FFMC,DMC,ISI,Classes,Region]])
+            result= ridge_model.predict(new_data_scaled)
+            
+            return render_template('home.html', result=result[0])
+        
+        except Exception as e:
+            return render_template('home.html', result=f"Error: {str(e)}")
+
     else:
         return render_template('home.html')
 def index():
